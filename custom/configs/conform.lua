171f8e79a7b375e7768b27fa1c619eda9a9d8e80
eljamm <fedi.jamoussi@protonmail.ch>
2024-04-12 01:46:21 +0100

    Formatting improvements

--- custom/configs/conform.lua
+++ custom/configs/conform.lua
@@ -4,27 +4,43 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
 
-    javascript = { "prettier" },
-    css = { "prettier" },
-    html = { "prettier" },
+    javascript = { { "prettierd", "prettier" } },
+    css = { { "prettierd", "prettier" } },
+    html = { { "prettierd", "prettier" } },
 
     sh = { "shfmt" },
+
+    python = function(bufnr)
+      if require("conform").get_formatter_info("ruff_format", bufnr).available then
+        return { "ruff_format" }
+      else
+        return { "isort", "black" }
+      end
+    end,
 
     go = {
       "golines",
       "gofumpt",
-      "goimports-reviser",
+      "goimports",
     },
 
     gdscript = { "gdformat" },
 
     nix = { "nixpkgs_fmt" },
+
+    rust = { "rustfmt" },
+
+    -- Fix common misspellings in source code on all filetypes
+    ["*"] = { "codespell" },
   },
 
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_fallback = true }
+  end,
 }
 
 require("conform").setup(options)
--- custom/init.lua
+++ custom/init.lua
@@ -1,9 +1,48 @@
 local autocmd = vim.api.nvim_create_autocmd
 
--- Default to relative numbers
+--- Default to relative numbers
 vim.opt.relativenumber = true
 
--- Go to Tab
+--- Toggle formatting on save
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
+
+--- Formats buffers using the attached language server clients
+--- or using `conform` as a fallback.
+--- (https://github.com/stevearc/conform.nvim/issues/316)
+local function format()
+  local buf_clients = vim.lsp.buf_get_clients()
+
+  -- Check LSP clients that support formatting
+  for _, client in pairs(buf_clients) do
+    if client.supports_method "textDocument/formatting" then
+      vim.lsp.buf.format { async = true }
+      return
+    end
+  end
+
+  -- Fallback on conform
+  require("conform").format()
+end
+
+vim.keymap.set("n", "<leader>fn", format, { desc = "Format Buffers (Native)" })
+
+--- Go to Tab
 for i = 1, 9, 1 do
   vim.keymap.set("n", string.format("<A-%s>", i), function()
     if vim.t.bufs[i] then
@@ -12,7 +51,7 @@ for i = 1, 9, 1 do
   end)
 end
 
--- Enable inline LSP hinting (nvim >= 0.10)
+--- Enable inline LSP hinting (nvim >= 0.10)
 if vim.fn.has "nvim-0.10" == 1 then
   autocmd("LspAttach", {
     desc = "Enable inlay hints",
