9477e78a37f570989454bd6e10dd29c04f37142e
eljamm <fedi.jamoussi@protonmail.ch>
2024-05-19 09:12:00 +0100

    editor.tabout: fix syntax and enable config
    For lazy.nvim, we use 'dependencies', not 'requires'.

--- lua/custom/plugins/editor/tabout.lua
+++ lua/custom/plugins/editor/tabout.lua
@@ -1,11 +1,12 @@
 return {
   {
     'abecodes/tabout.nvim',
-    requires = {
+    dependencies = { -- These are optional
       'nvim-treesitter/nvim-treesitter',
       'L3MON4D3/LuaSnip',
       'hrsh7th/nvim-cmp',
     },
+    config = true,
     event = 'InsertEnter', -- Set the event to 'InsertCharPre' for better compatibility
     priority = 1000,
   },
