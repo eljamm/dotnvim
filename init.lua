421c32673eab78669d638bfe01ef67b25d78f8d9
eljamm <fedi.jamoussi@protonmail.ch>
2024-05-27 20:30:31 +0100

    Enable Inlay Hints by default

--- init.lua
+++ init.lua
@@ -645,9 +645,13 @@ require('lazy').setup({
           --
           -- This may be unwanted, since they displace some of your code
           if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+            -- Toggle inlay hints
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
             end, '[T]oggle Inlay [H]ints')
+
+            -- Enable inlay hints by default
+            vim.lsp.inlay_hint.enable()
           end
         end,
       })
