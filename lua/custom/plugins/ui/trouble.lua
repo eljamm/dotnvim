00bee39d076780048eec266db140706f9ad48f88
eljamm <fedi.jamoussi@protonmail.ch>
2024-05-30 15:03:10 +0100

    Update plugins and use stable branch for `trouble`
    The new `trouble` version has been out from development and updated to
    the stable v3.0.0

--- lua/custom/plugins/ui/trouble.lua
+++ lua/custom/plugins/ui/trouble.lua
@@ -1,6 +1,6 @@
 return {
   'folke/trouble.nvim',
-  branch = 'dev', -- IMPORTANT!
+  branch = 'main',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     local actions = require 'telescope.actions'
