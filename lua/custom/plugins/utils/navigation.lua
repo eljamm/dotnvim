b9f77ff4736bf5cbc70bfb3d4c734b40e29ae3e2
eljamm <fedi.jamoussi@protonmail.ch>
2024-07-05 23:54:52 +0100

    grapple: print a message that the buffer was added

--- lua/custom/plugins/utils/navigation.lua
+++ lua/custom/plugins/utils/navigation.lua
@@ -7,7 +7,7 @@ return {
     event = { 'BufReadPost', 'BufNewFile' },
     cmd = 'Grapple',
     keys = {
-      { '<M-a>', '<CMD>Grapple toggle<CR>', desc = 'Grapple Toggle Tag' },
+      { '<M-a>', '<CMD>Grapple toggle<CR><CMD>echo "Grappled Buffer"<CR>', desc = 'Grapple Toggle Tag' },
       { '<M-e>', '<CMD>Grapple toggle_tags<CR>', desc = 'Grapple Open Tags Window' },
       { '<M-h>', '<CMD>Grapple cycle_tags next<CR>', desc = 'Grapple Cycle Next Tag' },
       { '<M-l>', '<CMD>Grapple cycle_tags prev<CR>', desc = 'Grapple Cycle Previous Tag' },
