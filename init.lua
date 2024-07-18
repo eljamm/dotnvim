5b3719afe231462c24747c2fdf4bcc3606e066df
eljamm <fedi.jamoussi@protonmail.ch>
2024-07-18 11:43:05 +0100

    telescope: add keymap to grep search results again

--- init.lua
+++ init.lua
@@ -486,6 +486,36 @@ require('lazy').setup({
                 -- freeze the current list and start a fuzzy search in the frozen list
                 ['<C-space>'] = require('telescope.actions').to_fuzzy_refine,
               },
+              n = {
+                -- Re-grep
+                -- https://stackoverflow.com/questions/77980402/is-there-a-way-to-grep-on-files-that-are-returned-by-telescopes-live-grep
+                ['<C-r>'] = {
+                  function(p_bufnr)
+                    -- send results to quick fix list
+                    require('telescope.actions').send_to_qflist(p_bufnr)
+                    local qflist = vim.fn.getqflist()
+                    local paths = {}
+                    local hash = {}
+                    for k in pairs(qflist) do
+                      local path = vim.fn.bufname(qflist[k]['bufnr']) -- extract path from quick fix list
+                      if not hash[path] then -- add to paths table, if not already appeared
+                        paths[#paths + 1] = path
+                        hash[path] = true -- remember existing paths
+                      end
+                    end
+                    -- -- show search scope with message
+                    -- vim.notify('find in ...\n  ' .. table.concat(paths, '\n  '))
+                    -- execute live_grep_args with search scope
+                    require('telescope').extensions.live_grep_args.live_grep_args { search_dirs = paths }
+                  end,
+                  type = 'action',
+                  opts = {
+                    nowait = true,
+                    silent = true,
+                    desc = 'Live grep on results',
+                  },
+                },
+              },
             },
             -- ... also accepts theme settings, for example:
             -- theme = "dropdown", -- use dropdown theme
