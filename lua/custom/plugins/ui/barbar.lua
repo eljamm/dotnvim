112cb78937a9cc793a4a1e4db4bbff6b105deeec
eljamm <fedi.jamoussi@protonmail.ch>
2024-07-20 08:50:08 +0100

    barbar: refactor configuration

--- lua/custom/plugins/ui/barbar.lua
+++ lua/custom/plugins/ui/barbar.lua
@@ -1,109 +1,116 @@
-local map = vim.api.nvim_set_keymap
-
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local exec_autocmds = vim.api.nvim_exec_autocmds
 
-local opts = function(desc)
-  return { desc = desc, noremap = true, silent = true }
-end
-
 return {
-  'romgrk/barbar.nvim',
-  event = 'BufEnter',
-  dependencies = {
-    -- Optional
-    'lewis6991/gitsigns.nvim', -- git status
-    'nvim-tree/nvim-web-devicons', -- file icons
-  },
-  init = function()
-    vim.g.barbar_auto_setup = false
-
-    -- Move to previous/next
-    map('n', '<A-j>', '<Cmd>BufferPrevious<CR>', opts 'Go to Previous Buffer')
-    map('n', '<A-k>', '<Cmd>BufferNext<CR>', opts 'Go to Next Buffer')
-    -- Re-order to previous/next
-    map('n', '<A-S-j>', '<Cmd>BufferMovePrevious<CR>', opts 'Move Buffer Left')
-    map('n', '<A-S-k>', '<Cmd>BufferMoveNext<CR>', opts 'Move Buffer Right')
-    -- Goto buffer in position...
-    map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts 'Go to Buffer 1')
-    map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts 'Go to Buffer 2')
-    map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts 'Go to Buffer 3')
-    map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts 'Go to Buffer 4')
-    map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts 'Go to Buffer 5')
-    map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts 'Go to Buffer 6')
-    map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts 'Go to Buffer 7')
-    map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts 'Go to Buffer 8')
-    map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts 'Go to Buffer 9')
-    map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts 'Go to Last Buffer')
-    -- Pin/unpin buffer
-    map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts '[P]in Buffer')
-    -- Close buffer
-    map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts '[C]lose Buffer')
-    map('n', '<A-x>', '<Cmd>BufferClose!<CR>', opts 'Exit Buffer')
-    -- Wipeout buffer
-    --                 :BufferWipeout
-    -- Close commands
-    --                 :BufferCloseAllButCurrent
-    --                 :BufferCloseAllButPinned
-    --                 :BufferCloseAllButCurrentOrPinned
-    --                 :BufferCloseBuffersLeft
-    --                 :BufferCloseBuffersRight
-    -- Magic buffer-picking mode
-    map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts '[P]ick Buffer')
-    -- Sort automatically by...
-    map('n', '<Space>bon', '<Cmd>BufferOrderByBufferNumber<CR>', opts '[B]uffer [O]rder [N]umber')
-    map('n', '<Space>bod', '<Cmd>BufferOrderByDirectory<CR>', opts '[B]uffer [O]rder [D]irectory')
-    map('n', '<Space>bol', '<Cmd>BufferOrderByLanguage<CR>', opts '[B]uffer [O]rder [L]anguage')
-    map('n', '<Space>bow', '<Cmd>BufferOrderByWindowNumber<CR>', opts '[B]uffer [O]rder [W]indow')
-
-    -- Other:
-    -- :BarbarEnable - enables barbar (enabled by default)
-    -- :BarbarDisable - very bad command, should never be used
-
-    -- Save buffer location with persisted.nvim
-    vim.opt.sessionoptions:append 'globals'
-    autocmd({ 'User' }, {
-      pattern = 'PersistedSavePre',
-      group = augroup('PersistedHooks', {}),
-      callback = function()
-        exec_autocmds('User', { pattern = 'SessionSavePre' })
-      end,
-    })
-  end,
-  opts = {
-    -- Enable/disable animations
-    animation = false,
-
-    -- -- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
-    -- hide = { extensions = true },
-
-    -- If true, new buffers will be inserted at the start/end of the list.
-    -- Default is to insert after current buffer.
-    insert_at_end = false,
-    insert_at_start = false,
-
-    -- A buffer to this direction will be focused (if it exists) when closing the current buffer.
-    -- Valid options are 'left' (the default), 'previous', and 'right'
-    focus_on_close = 'right',
-
-    sidebar_filetypes = {
-      -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
-      NvimTree = true,
-      -- Or, specify the event which the sidebar executes when leaving:
-      ['neo-tree'] = { event = 'BufWipeout' },
-      -- -- Or, specify the text used for the offset:
-      -- undotree = {
-      --   text = 'undotree',
-      --   align = 'center', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
-      -- },
-      -- -- Or, specify all three
-      -- Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
+  {
+    'romgrk/barbar.nvim',
+    event = 'BufEnter',
+    dependencies = {
+      -- Optional
+      'lewis6991/gitsigns.nvim', -- git status
+      'nvim-tree/nvim-web-devicons', -- file icons
     },
-    -- New buffer letters are assigned in this order. This order is
-    -- optimal for the QWERTZ keyboard layout but might need adjustment
-    -- for other layouts.
-    letters = 'asdfjklöghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+    keys = {
+      -- Move to previous/next
+      { '<A-j>', '<CMD>BufferPrevious<CR>', desc = 'Go to Previous Buffer', noremap = true, silent = true },
+      { '<A-k>', '<CMD>BufferNext<CR>', desc = 'Go to Next Buffer', noremap = true, silent = true },
+      -- Re-order to previous/next
+      { '<A-S-j>', '<CMD>BufferMovePrevious<CR>', desc = 'Go to Previous Buffer', noremap = true, silent = true },
+      { '<A-S-k>', '<CMD>BufferMoveNext<CR>', desc = 'Go to Next Buffer', noremap = true, silent = true },
+      -- Goto buffer in position...
+      { '<A-1>', '<Cmd>BufferGoto 1<CR>', desc = 'Go to Buffer 1', noremap = true, silent = true },
+      { '<A-2>', '<Cmd>BufferGoto 2<CR>', desc = 'Go to Buffer 2', noremap = true, silent = true },
+      { '<A-3>', '<Cmd>BufferGoto 3<CR>', desc = 'Go to Buffer 3', noremap = true, silent = true },
+      { '<A-4>', '<Cmd>BufferGoto 4<CR>', desc = 'Go to Buffer 4', noremap = true, silent = true },
+      { '<A-5>', '<Cmd>BufferGoto 5<CR>', desc = 'Go to Buffer 5', noremap = true, silent = true },
+      { '<A-6>', '<Cmd>BufferGoto 6<CR>', desc = 'Go to Buffer 6', noremap = true, silent = true },
+      { '<A-7>', '<Cmd>BufferGoto 7<CR>', desc = 'Go to Buffer 7', noremap = true, silent = true },
+      { '<A-8>', '<Cmd>BufferGoto 8<CR>', desc = 'Go to Buffer 8', noremap = true, silent = true },
+      { '<A-9>', '<Cmd>BufferGoto 9<CR>', desc = 'Go to Buffer 9', noremap = true, silent = true },
+      { '<A-0>', '<Cmd>BufferLast<CR>', desc = 'Go to Last Buffer', noremap = true, silent = true },
+      -- Pin/unpin buffer
+      { '<A-p>', '<Cmd>BufferPin<CR>', desc = '[P]in Buffer', noremap = true, silent = true },
+      -- Close buffer
+      { '<A-c>', '<Cmd>BufferClose<CR>', desc = '[C]lose Buffer', noremap = true, silent = true },
+      { '<A-x>', '<Cmd>BufferClose!<CR>', desc = 'Exit Buffer', noremap = true, silent = true },
+      -- Wipeout buffer
+      --                 :BufferWipeout
+      -- Close commands
+      --                 :BufferCloseAllButCurrent
+      --                 :BufferCloseAllButPinned
+      --                 :BufferCloseAllButCurrentOrPinned
+      --                 :BufferCloseBuffersLeft
+      --                 :BufferCloseBuffersRight
+      -- Magic buffer-picking mode
+      { '<C-p>', '<Cmd>BufferPick<CR>', desc = '[P]ick Buffer', noremap = true, silent = true },
+      -- Sort automatically by...
+      --stylua: ignore start
+      { '<Space>bon', '<Cmd>BufferOrderByBufferNumber<CR>', desc = '[B]uffer [O]rder [N]umber', noremap = true, silent = true  },
+      { '<Space>bod', '<Cmd>BufferOrderByDirectory<CR>', desc = '[B]uffer [O]rder [D]irectory', noremap = true, silent = true  },
+      { '<Space>bol', '<Cmd>BufferOrderByLanguage<CR>', desc = '[B]uffer [O]rder [L]anguage', noremap = true, silent = true  },
+      { '<Space>bow', '<Cmd>BufferOrderByWindowNumber<CR>', desc = '[B]uffer [O]rder [W]indow', noremap = true, silent = true  },
+      --stylua: ignore end
+      -- Other:
+      -- :BarbarEnable - enables barbar (enabled by default)
+      -- :BarbarDisable - very bad command, should never be used
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+
+      -- Save buffer location
+      vim.opt.sessionoptions:append 'globals'
+      if vim.g.loaded_persisted then
+        autocmd({ 'User' }, {
+          pattern = 'PersistedSavePre',
+          group = augroup('PersistedHooks', {}),
+          callback = function()
+            exec_autocmds('User', { pattern = 'SessionSavePre' })
+          end,
+        })
+      else
+        autocmd({ 'User' }, {
+          pattern = 'PersistenceSavePre',
+          callback = function()
+            exec_autocmds('User', { pattern = 'SessionSavePre' })
+          end,
+        })
+      end
+    end,
+    opts = {
+      -- Enable/disable animations
+      animation = false,
+
+      -- -- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
+      -- hide = { extensions = true },
+
+      -- If true, new buffers will be inserted at the start/end of the list.
+      -- Default is to insert after current buffer.
+      insert_at_end = true,
+      insert_at_start = false,
+
+      -- A buffer to this direction will be focused (if it exists) when closing the current buffer.
+      -- Valid options are 'left' (the default), 'previous', and 'right'
+      focus_on_close = 'right',
+
+      sidebar_filetypes = {
+        -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+        NvimTree = true,
+        -- Or, specify the event which the sidebar executes when leaving:
+        ['neo-tree'] = { event = 'BufWipeout' },
+        -- -- Or, specify the text used for the offset:
+        -- undotree = {
+        --   text = 'undotree',
+        --   align = 'center', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+        -- },
+        -- -- Or, specify all three
+        -- Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
+      },
+      -- New buffer letters are assigned in this order. This order is
+      -- optimal for the QWERTZ keyboard layout but might need adjustment
+      -- for other layouts.
+      letters = 'asdfjklöghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
-  version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
