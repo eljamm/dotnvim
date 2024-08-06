429ed13d4feee16c821e591a3a694361ad68279b
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-06 23:48:42 +0100

    neo-tree: use normal mode for renaming files

--- lua/custom/plugins/ui/neo-tree.lua
+++ lua/custom/plugins/ui/neo-tree.lua
@@ -21,6 +21,20 @@ return {
       filesystem = {
         follow_current_file = { enabled = true },
       },
+      event_handlers = {
+        -- Use normal mode for renaming files
+        {
+          event = 'neo_tree_popup_input_ready',
+          ---@param args { bufnr: integer, winid: integer }
+          handler = function(args)
+            -- enter input popup with normal mode by default.
+            vim.cmd 'stopinsert'
+            -- map <esc> to enter normal mode (by default closes prompt)
+            -- don't forget `opts.buffer` to specify the buffer of the popup.
+            vim.keymap.set('i', '<esc>', vim.cmd.stopinsert, { noremap = true, buffer = args.bufnr })
+          end,
+        },
+      },
     },
     keys = {
       { '<C-n>', '<CMD>Neotree toggle<CR>', mode = { 'n', 'x', 't' }, desc = 'Toggle [N]eoTree', nowait = true },
