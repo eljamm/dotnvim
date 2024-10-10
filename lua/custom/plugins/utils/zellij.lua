dd49e679bf862227ae068b4c4f7d3fb369a0d642
eljamm <fedi.jamoussi@protonmail.ch>
2024-10-10 14:05:43 +0100

    zellij: disable navigating down

--- lua/custom/plugins/utils/zellij.lua
+++ lua/custom/plugins/utils/zellij.lua
@@ -1,10 +1,11 @@
 return {
   {
     'https://git.sr.ht/~swaits/zellij-nav.nvim',
+    enabled = false,
     event = 'VeryLazy',
     keys = {
       { '<c-h>', '<CMD>ZellijNavigateLeft<CR>', { silent = true, desc = 'navigate left' } },
-      { '<c-j>', '<CMD>ZellijNavigateDown<CR>', { silent = true, desc = 'navigate down' } },
+      -- { '<c-j>', '<CMD>ZellijNavigateDown<CR>', { silent = true, desc = 'navigate down' } },
       { '<c-k>', '<CMD>ZellijNavigateUp<CR>', { silent = true, desc = 'navigate up' } },
       { '<c-l>', '<CMD>ZellijNavigateRight<CR>', { silent = true, desc = 'navigate right' } },
     },
