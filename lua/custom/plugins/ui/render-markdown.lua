dab07e66daa42f73f73972fb47d465c5a1603f66
eljamm <fedi.jamoussi@protonmail.ch>
2024-10-25 11:53:36 +0100

    feat: let render-markdown lazy load itself
    - Remove ft, cmd and keys settings
    - Change mapping to `<leader>mr`
    - Disable latex support

--- lua/custom/plugins/ui/render-markdown.lua
+++ lua/custom/plugins/ui/render-markdown.lua
@@ -1,13 +1,14 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  ft = 'md',
-  cmd = { 'RenderMarkdown' },
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
   },
   version = '*',
-  keys = {
-    { '<leader>rm', '<CMD>RenderMarkdown toggle<CR>', desc = '[R]ender [M]arkdown' },
+  opts = {
+    latex = { enabled = false },
   },
+  init = function()
+    vim.keymap.set('n', '<leader>mr', '<CMD>RenderMarkdown toggle<CR>', { desc = '[M]arkdown [R]ender' })
+  end,
 }
