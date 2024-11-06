f9679cace4623e1064a79e1e59bf495766629902
eljamm <fedi.jamoussi@protonmail.ch>
2024-11-06 16:36:25 +0100

    feat(autopairs): ignore if next char is '{'

--- lua/custom/plugins/editor/autopairs.lua
+++ lua/custom/plugins/editor/autopairs.lua
@@ -9,7 +9,12 @@ return {
       disable_filetype = { 'TelescopePrompt', 'vim' },
     },
     config = function()
-      require('nvim-autopairs').setup {}
+      require('nvim-autopairs').setup {
+        -- Don't add pairs if it already has a close pair in the same line
+        enable_check_bracket_line = false,
+
+        ignored_next_char = [=[[%w%%%'%[%"%.%`%$%{]]=],
+      }
       -- If you want to automatically add `(` after selecting a function or method
       local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
       local cmp = require 'cmp'
