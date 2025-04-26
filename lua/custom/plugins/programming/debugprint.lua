60cb5db015a77975558517f123c7dbb978338718
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-26 16:30:05 +0200

    feat: add lua-console for debugging

--- lua/custom/plugins/programming/debugprint.lua
+++ lua/custom/plugins/programming/debugprint.lua
@@ -1,10 +1,22 @@
 return {
-  'andrewferrier/debugprint.nvim',
-  event = 'LazyFile',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
+  {
+    'andrewferrier/debugprint.nvim',
+    event = 'LazyFile',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    -- stable releases
+    version = '*',
+    opts = {},
   },
-  -- stable releases
-  version = '*',
-  opts = {},
+  {
+    'yarospace/lua-console.nvim',
+    enabled = false,
+    lazy = true,
+    keys = {
+      { '`', desc = 'Lua-console - toggle' },
+      { '<Leader>`', desc = 'Lua-console - attach to buffer' },
+    },
+    opts = {},
+  },
 }
