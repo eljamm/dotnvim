81481a6b1030dddafc04e8113e3b2d0f1809a758
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-26 22:02:58 +0200

    feat: add yankAssassin for freezing yanks

--- lua/custom/plugins/productivity/init.lua
+++ lua/custom/plugins/productivity/init.lua
@@ -9,4 +9,19 @@ return {
     dependencies = { 'MunifTanjim/nui.nvim' },
     opts = {},
   },
+  -- Don't let the cursor move while Yanking
+  {
+    'svban/YankAssassin.nvim',
+    config = function()
+      require('YankAssassin').setup {
+        -- For both options: if true, autocmds are used.
+        -- Whenever 'y' is used in normal mode, the cursor doesn't move to start.
+        auto_normal = true,
+        auto_visual = true,
+      }
+      -- Optional Mappings
+      vim.keymap.set({ 'x', 'n' }, 'gy', '<Plug>(YADefault)', { silent = true })
+      vim.keymap.set({ 'x', 'n' }, '<leader>y', '<Plug>(YANoMove)', { silent = true })
+    end,
+  },
 }
