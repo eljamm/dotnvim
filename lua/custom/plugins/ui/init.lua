f80f3fe7158702f7c2d93f18dd45625a0beaba28
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-27 18:54:54 +0200

    feat(ts-context): add toggle keymap

--- lua/custom/plugins/ui/init.lua
+++ lua/custom/plugins/ui/init.lua
@@ -1,11 +1,18 @@
 return {
   -- show code context
-  -- TODO: add keymaps for toggling
   {
     'nvim-treesitter/nvim-treesitter-context',
     event = 'LazyFile',
+    cmd = { 'TSContextEnable', 'TSContextDisable', 'TSContextToggle' },
+    keys = {
+      { '<leader>cl', '<CMD>TSContextToggle<CR>', { silent = true, desc = '[C]ontext [L]ens' } },
+    },
     opts = {
       mode = 'topline', -- Line used to calculate context. Choices: 'cursor', 'topline'
     },
+    init = function()
+      -- hide, by default
+      vim.cmd 'TSContextDisable'
+    end,
   },
 }
