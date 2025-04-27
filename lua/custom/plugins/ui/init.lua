cd26f1359c7fa75f1dbd06e9c1202d50290fe201
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-27 13:53:20 +0200

    feat(ui): add nvim-treesitter-context

--- lua/custom/plugins/ui/init.lua
+++ lua/custom/plugins/ui/init.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  -- show code context
+  -- TODO: add keymaps for toggling
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'LazyFile',
+    opts = {
+      mode = 'topline', -- Line used to calculate context. Choices: 'cursor', 'topline'
+    },
+  },
+}
