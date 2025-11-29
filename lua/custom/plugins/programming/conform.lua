b0396ba55dbb8967a9bf5f0368edfe611f02a814
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-29 10:09:49 +0100

    refactor(conform): pretty format

--- lua/custom/plugins/programming/conform.lua
+++ lua/custom/plugins/programming/conform.lua
@@ -1,20 +1,4 @@
----@param bufnr integer
----@param ... string
----@return string
-local function first(bufnr, ...)
-  local conform = require 'conform'
-  for i = 1, select('#', ...) do
-    local formatter = select(i, ...)
-    if conform.get_formatter_info(formatter, bufnr).available then
-      return formatter
-    end
-  end
-  return select(1, ...)
-end
-
-local prettyFormat = function(bufnr)
-  return { first(bufnr, 'prettierd', 'prettier') }
-end
+local prettyFormat = { 'prettierd', 'prettier', stop_after_first = true }
 
 return {
   'stevearc/conform.nvim',
